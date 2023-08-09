ad377592c7aaad065d557a1fa10d53efa40a67a0
Stephen Haase <shaase@gmail.com>
2023-08-09 12:13:30 -0600

    adding image

--- app.py
+++ app.py
@@ -7,12 +7,14 @@ from diffusers import DiffusionPipeline
 app = Potassium("my_app")
 
 # @app.init runs at startup, and loads models into the app's context
+
+
 @app.init
 def init():
     base = DiffusionPipeline.from_pretrained(
-        "stabilityai/stable-diffusion-xl-base-1.0", 
-        torch_dtype=torch.float16, 
-        use_safetensors=True, 
+        "stabilityai/stable-diffusion-xl-base-1.0",
+        torch_dtype=torch.float16,
+        use_safetensors=True,
         variant="fp16"
     )
     base.to("cuda")
@@ -36,36 +38,58 @@ def init():
     return context
 
 # @app.handler runs for every call
+
+
 @app.handler("/")
 def handler(context: dict, request: Request) -> Response:
-    prompt = request.json.get("prompt")
-    num_steps = request.json.get("num_steps", 50)
-    high_noise_frac = request.json.get("high_noise_frac", 0.8)
-
     model = context.get("model")
     refiner = context.get("refiner")
-    image = model(
-        prompt=prompt,
-        num_inference_steps=num_steps,
-        denoising_end=high_noise_frac,
-        output_type="latent",
-    ).images
+
+    prompt = request.json.get("prompt")
+    width = request.json.get("width")
+    height = request.json.get("height")
+    num_steps = request.json.get("num_steps", 50)
+    high_noise_frac = request.json.get("high_noise_frac", 0.8)
+    init_image = request.json.get("image")
+
+    if init_image:
+        image = model(
+            prompt=prompt,
+            width=width,
+            height=height,
+            num_inference_steps=num_steps,
+            denoising_end=high_noise_frac,
+            image=init_image,
+            output_type="latent",
+        ).images
+    else:
+        image = model(
+            prompt=prompt,
+            width=width,
+            height=height,
+            num_inference_steps=num_steps,
+            denoising_end=high_noise_frac,
+            output_type="latent",
+        ).images
 
     image = refiner(
         prompt=prompt,
+        width=width,
+        height=height,
         num_inference_steps=num_steps,
         denoising_start=high_noise_frac,
         image=image,
     ).images[0]
 
-    buffered  = BytesIO()
-    image.save(buffered ,format="JPEG")
+    buffered = BytesIO()
+    image.save(buffered, format="JPEG")
     output = base64.b64encode(buffered .getvalue()).decode('utf-8')
 
     return Response(
-        json = {"output": output}, 
+        json={"output": output},
         status=200
     )
+
 
 if __name__ == "__main__":
     app.serve()
